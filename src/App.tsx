f4522e6eac82b4cbd5eae48d7d4b677b23f16eb7
Frederico Stevanato Rocha <fredzstevanato@gmail.com>
2021-07-13 21:13:16 -0300

    fix: aditional configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,40 @@
-import React from 'react';
+//import styled from 'styled-components';
+import { GlobalStyle } from './styles/global';
+import { Header } from './components/Header';
+import { CallAction } from './components/CallAction';
+import { Feature } from './components/Feature';
+import { Footer } from './components/Footer';
+import { Section } from './components/Section';
+import { RegisterModal } from './components/RegisterModal'
+import Modal from 'react-modal';
+import { useState } from 'react';
+
+Modal.setAppElement('#root');
 
 function App() {
+  const [isRegisterModalOpen, setIsRegisterModalOpen] = useState(false);
+
+  function handleOpenRegisterModal() {
+    setIsRegisterModalOpen(!isRegisterModalOpen);
+    console.log(isRegisterModalOpen)
+  }
+
+  function handleCloseRegisterModal() {
+    setIsRegisterModalOpen(false);
+    console.log(isRegisterModalOpen)
+
+  }
   return (
-    <h1>Hello World</h1>
+    <>
+      <Header />
+      <CallAction onOpenRegisterModal={handleOpenRegisterModal} />
+      <RegisterModal isOpen={isRegisterModalOpen} onRequestClose={handleCloseRegisterModal} />
+      <Feature />
+      <Footer />
+      <GlobalStyle />
+      <Section />
+
+    </>
   );
 }
 
