46af66379a8027efa5079852a01181f6e14765f9
Frederico Stevanato Rocha <fredzstevanato@gmail.com>
2021-07-13 21:12:24 -0300

    chore: add component Section (not funcional)

---
+++ src/components/Section/styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import backgroundSection from '../../assets/images/background-section.svg';
+
+
+export const Container = styled.div`
+      background-image: var(--background) url(${backgroundSection}) no-repeat right top;
+
+`
